43603efe9c8283c270e92e1b486404910f6e4244
Rangineni Sucharitha <ranginenisucharitharao@gmail.com>
2022-04-22 14:43:29 +0530

    added actors list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Sample file</h1>
+      <h1>List of Actors</h1>
+      <h2>Prabhas</h2>
+      <h2>Ram Charan Tej</h2>
+      <h2>Chireenjevi</h2>
+      <h2>Ram</h2>
+      <h2>Mahesh Babu</h2>
+      <h2>N.T.R</h2>
     </div>
   );
 }
